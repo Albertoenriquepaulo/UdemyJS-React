9c2e77e355fae96e9141e0059a741b51509b7953
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-08 16:08:55 +0100

    242. Comenzando nuestro primer proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+// Enlace del proyecto donde esta el contenido de index.css
+// https://gist.github.com/juanpablogdl/194c15d376391a67c46dd08817bf9ae4
+
+import React, { Fragment } from 'react';
+import Header from './component/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi World</h1>
-    </div>
+    // <div className="App">
+    //   <Header />
+    //   <Header />
+    // </div>
+    // Sid escomentamos lo de arriba haria lo mismo que lo de Fragment
+    // Pero para usar Fragment hay que incluirlo en el import
+    <Fragment>
+      <Header />
+      <Header />
+    </Fragment>
   );
 }
 
