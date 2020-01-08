02dbbb3688f9ff1c75ba73bb0e72fc43e4a2deab
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-08 17:59:56 +0100

    249. Mostrar mensaje de error

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Form = ({ cantidad, guardarCantidad, plazo, guardarPlazo }) => {
     // Definir el State
@@ -18,49 +18,56 @@ const Form = ({ cantidad, guardarCantidad, plazo, guardarPlazo }) => {
         // Validar 
         if (cantidad === 0 || plazo === '') {
             guardarError(true);
-        } else {
-            guardarError(false);;
+            return;
         }
+        // Eliminar el error previo
+        guardarError(false);
+
         // Realizar Cotización
     }
 
 
     return (
-        <form onSubmit={calcularPrestamo}>
-            {cantidad}
-            {plazo}
-            <div className="row">
-                <div>
-                    <label>Cantidad Prestamo</label>
-                    <input
-                        className="u-full-width"
-                        type="number"
-                        placeholder="Ejemplo: 3000"
-                        onChange={(e) => guardarCantidad(+e.target.value)}
-                    />
+        <Fragment>
+            <form onSubmit={calcularPrestamo}>
+                {cantidad}
+                {plazo}
+                <div className="row">
+                    <div>
+                        <label>Cantidad Prestamo</label>
+                        <input
+                            className="u-full-width"
+                            type="number"
+                            placeholder="Ejemplo: 3000"
+                            onChange={(e) => guardarCantidad(+e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Plazo para Pagar</label>
+                        <select
+                            className="u-full-width"
+                            onChange={(e) => guardarPlazo(+e.target.value)}
+                        >
+                            <option value="">Seleccionar</option>
+                            <option value="3">3 meses</option>
+                            <option value="6">6 meses</option>
+                            <option value="12">12 meses</option>
+                            <option value="24">24 meses</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input
+                            type="submit"
+                            value="Calcular"
+                            className="button-primary u-full-width"
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label>Plazo para Pagar</label>
-                    <select
-                        className="u-full-width"
-                        onChange={(e) => guardarPlazo(+e.target.value)}
-                    >
-                        <option value="">Seleccionar</option>
-                        <option value="3">3 meses</option>
-                        <option value="6">6 meses</option>
-                        <option value="12">12 meses</option>
-                        <option value="24">24 meses</option>
-                    </select>
-                </div>
-                <div>
-                    <input
-                        type="submit"
-                        value="Calcular"
-                        className="button-primary u-full-width"
-                    />
-                </div>
-            </div>
-        </form>
+            </form>
+            {(error) ? <p className="error">Todos los campos son obligatorios</p> : null}
+
+        </Fragment>
+
     );
 }
 
